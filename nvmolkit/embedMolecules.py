56e6e0e385f38466e00dcea44faa2c84819ec9b9
Kevin Boyd <kboyd@nvidia.com>
2026-09-18 14:11:53 -0400

    Format ETKDG precision tests

--- nvmolkit/embedMolecules.py
+++ nvmolkit/embedMolecules.py
@@ -26,8 +26,8 @@ if TYPE_CHECKING:
 
 __all__ = ["EmbedMolecules"]
 
-from nvmolkit.types import CoordinateOutput, Device3DResult, HardwareOptions, PrecisionMode
 from nvmolkit import _embedMolecules  # type: ignore
+from nvmolkit.types import CoordinateOutput, Device3DResult, HardwareOptions, PrecisionMode
 
 
 @overload
